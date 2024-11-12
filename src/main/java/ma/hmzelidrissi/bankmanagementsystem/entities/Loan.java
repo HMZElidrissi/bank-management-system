b4de488b959cb2415e9d3e188c637484bb2563ea
Hamza El Idrissi <hamza.ezzharelidrissi1@gmail.com>
2024-11-12 16:46:00 +0100

    update loan attributes

--- src/main/java/ma/hmzelidrissi/bankmanagementsystem/entities/Loan.java
+++ src/main/java/ma/hmzelidrissi/bankmanagementsystem/entities/Loan.java
@@ -1,22 +1,62 @@
 package ma.hmzelidrissi.bankmanagementsystem.entities;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import ma.hmzelidrissi.bankmanagementsystem.enums.LoanStatus;
+import ma.hmzelidrissi.bankmanagementsystem.enums.LoanType;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "loans")
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Loan {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private double principal; // the initial amount of money
+
+    private int age;
+
+    private double monthlyIncome;
+
+    private int creditScore;
+
+    private double debtRatio;
+
+    private String collateralDescription;
+
+    private LocalDate accountOpeningDate;
+
+    private double amount;
+
+    @Enumerated(EnumType.STRING)
+    private LoanType type;
+
     private double interestRate;
-    private int termMonths;
-    @ManyToOne
+
+    private int termMonths; // term in months
+
+    private double monthlyPayment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
-    private boolean approved;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reviewed_by")
+    private User reviewedBy;
+
+    private String rejectionReason;
+
+    @Enumerated(EnumType.STRING)
+    private LoanStatus status;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
 }
