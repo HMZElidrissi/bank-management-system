c550174abf80caeee9f7c5a3036ba3fcc0965ebf
hmzelidrissi <hamza.ezzharelidrissi1@gmail.com>
2025-01-13 09:54:49 +0100

    using ResponseCookie from Spring instead of Jakarta Cookie

--- src/main/java/ma/hmzelidrissi/bankmanagementsystem/config/CookieUtil.java
+++ src/main/java/ma/hmzelidrissi/bankmanagementsystem/config/CookieUtil.java
@@ -1,47 +1,69 @@
 package ma.hmzelidrissi.bankmanagementsystem.config;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.Arrays;
 
 @Component
 public class CookieUtil {
-    @Value("${application.jwt.cookie.name:jwt-token}")
-    private String cookieName;
+  @Value("${application.jwt.cookie.name:jwt-token}")
+  private String cookieName;
 
-    @Value("${application.jwt.cookie.expiry:86400}")
-    private int cookieExpiry;
+  @Value("${application.jwt.cookie.expiry:86400}")
+  private int cookieExpiry;
 
-    public void createCookie(HttpServletResponse response, String token) {
-        Cookie cookie = new Cookie(cookieName, token);
-        cookie.setHttpOnly(true); // HttpOnly flag makes sure that the cookie is not accessible via JavaScript
-        // cookie.setSecure(true); # Uncomment this line when deploying to production
-        cookie.setPath("/");
-        cookie.setDomain("localhost");
-        cookie.setMaxAge(cookieExpiry);
-        response.addCookie(cookie);
+  @Value("${application.jwt.cookie.secure:true}")
+  private boolean isSecure;
+
+  @Value("${application.jwt.cookie.same-site:None}")
+  private String sameSite;
+
+  @Value("${application.jwt.cookie.domain:localhost}")
+  private String domain;
+
+  public void createCookie(HttpServletResponse response, String token) {
+    ResponseCookie cookie =
+        ResponseCookie.from(cookieName, token)
+            .httpOnly(true)
+            .secure(isSecure)
+            .path("/")
+            .domain(domain)
+            .maxAge(Duration.ofSeconds(cookieExpiry))
+            .sameSite(sameSite)
+            .build();
+
+    response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+  }
+
+  public void clearCookie(HttpServletResponse response) {
+    ResponseCookie cookie =
+        ResponseCookie.from(cookieName, "")
+            .httpOnly(true)
+            .secure(isSecure)
+            .path("/")
+            .domain(domain)
+            .maxAge(0)
+            .sameSite(sameSite)
+            .build();
+
+    response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+  }
+
+  public String extractToken(HttpServletRequest request) {
+    if (request.getCookies() == null) {
+      return null;
     }
 
-    public void clearCookie(HttpServletResponse response) {
-        Cookie cookie = new Cookie(cookieName, null);
-        cookie.setHttpOnly(true);
-        // cookie.setSecure(true);
-        cookie.setPath("/");
-        cookie.setMaxAge(0);
-        response.addCookie(cookie);
-    }
-
-    public String extractToken(HttpServletRequest request) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookieName.equals(cookie.getName())) {
-                    return cookie.getValue();
-                }
-            }
-        }
-        return null;
-    }
+    return Arrays.stream(request.getCookies())
+        .filter(cookie -> cookieName.equals(cookie.getName()))
+        .map(jakarta.servlet.http.Cookie::getValue)
+        .findFirst()
+        .orElse(null);
+  }
 }
